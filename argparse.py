521e86265126612c792045098d782bdf4cce9c39
John Polo <68556651+John-Polo@users.noreply.github.com>
2021-08-02 11:47:06 -0400

    Delete argparse.py
    Unused.

--- argparse.py
+++
@@ -1,6 +0,0 @@
-parser = argparse.ArgumentParser()
-#parser.add_argument('-i', '--input', required=True, 
-#                    help='path to the input data')
-parser.add_argument('-t', '--threshold', default=0.965, type=float,
-                    help='score threshold for discarding detection')
-args = vars(parser.parse_args())
